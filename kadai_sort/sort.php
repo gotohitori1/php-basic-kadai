57c10a9ec59ea0159722c5d5b1862082accfc056
Shota Kiyonaga <148308217+gotohitori1@users.noreply.github.com>
2026-07-16 09:30:08 +0900

    Update sort.php

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,19 +9,35 @@
 
   <p>
     <?php
-        // ここにコードを書いていく
-        $nums = [15, 4, 18, 23, 10 ];
-        echo "昇順にソートします<br>";
-        sort($nums);
-        foreach ($nums as $sort_2way) {
-          echo $sort_2way . "<br>";
+        function sort_2way(&$array, $order)
+        {
+          if ($order == "asc") {
+          // 昇順
+          sort($array);
+          } else {
+          // 降順
+          rsort($array);
+          }
         }
-        echo "降順にソートします<br>";
-        rsort($nums);
-        foreach ($nums as $sort_2way) {
-          echo $sort_2way . "<br>";
-        }
+
+      $data = [15, 4, 18, 23, 10];
+
+      // 昇順
+      echo "昇順にソートします<br>";
+      $asc = $data;
+      sort_2way($asc, "asc");
+      foreach ($asc as $value) {
+        echo $value . "<br>";
+      }
+
+      echo "<br>";
+      // 降順
+      echo "降順にソートします<br>";
+      $desc = $data;
+      sort_2way($desc, "desc");
+      foreach ($desc as $value) {
+        echo $value . "<br>";
+      }
     ?>
   </p>
-
 </body>
