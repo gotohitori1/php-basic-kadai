7abc38ceda1a5ee9bc9977343513c44cb8dfc6c6
Shota Kiyonaga <148308217+gotohitori1@users.noreply.github.com>
2026-07-16 09:21:05 +0900

    Update sort.php

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -13,13 +13,13 @@
         $nums = [15, 4, 18, 23, 10 ];
         echo "昇順にソートします<br>";
         sort($nums);
-        foreach ($nums as $num) {
-          echo $num . "<br>";
+        foreach ($nums as $sort_2way) {
+          echo $sort_2way . "<br>";
         }
         echo "降順にソートします<br>";
         rsort($nums);
-        foreach ($nums as $num) {
-          echo $num . "<br>";
+        foreach ($nums as $sort_2way) {
+          echo $sort_2way . "<br>";
         }
     ?>
   </p>
